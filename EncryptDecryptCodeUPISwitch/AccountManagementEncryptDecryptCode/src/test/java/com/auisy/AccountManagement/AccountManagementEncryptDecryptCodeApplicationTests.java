22611d5161ec5c0280c7c8913f09023f278b50ac
TEJAS12Oct <jawaletejas1299@gmail.com>
2026-05-27 13:22:49 +0530

    Encryption Decryption Code

---
+++ EncryptDecryptCodeUPISwitch/AccountManagementEncryptDecryptCode/src/test/java/com/auisy/AccountManagement/AccountManagementEncryptDecryptCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auisy.AccountManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementEncryptDecryptCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
